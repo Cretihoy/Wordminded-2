aa057b0458cf8d2d2580f15f564ddd135bd87f84
cretihoy <sergzhio238@gmail.com>
2023-10-23 17:33:25 +0300

    create dimen file

---
+++ app/src/main/java/com/cretihoy/wordminded2/presentation/theme/Dimen.kt
@@ -0,0 +1,10 @@
+package com.cretihoy.wordminded2.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+const val EMPTY_STRING = ""
+
+val spacingSmall = 8.dp
+val spacingMedium = 16.dp
+val spacingLarge = 32.dp
+val minWidth = 96.dp
