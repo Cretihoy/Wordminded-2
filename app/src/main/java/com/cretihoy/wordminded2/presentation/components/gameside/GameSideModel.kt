8649624873d1282429157c79815836364b0ced39
cretihoy <sergzhio238@gmail.com>
2023-10-24 22:37:34 +0300

    fix game side model and view

--- app/src/main/java/com/cretihoy/wordminded2/presentation/components/gameside/GameSideModel.kt
+++ app/src/main/java/com/cretihoy/wordminded2/presentation/components/gameside/GameSideModel.kt
@@ -3,10 +3,12 @@ package com.cretihoy.wordminded2.presentation.components.gameside
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.cretihoy.wordminded2.presentation.components.button.ButtonModel
+import com.cretihoy.wordminded2.presentation.components.text.TextModel
 
 data class GameSideModel(
     val task: ButtonModel? = null,
     val letter: ButtonModel? = null,
+    val name: TextModel? = null,
     val isHorizontal: MutableState<Boolean> = mutableStateOf(false),
     val isInfinityGame: MutableState<Boolean> = mutableStateOf(false)
 )
--- app/src/main/java/com/cretihoy/wordminded2/presentation/components/gameside/GameSideView.kt
+++ app/src/main/java/com/cretihoy/wordminded2/presentation/components/gameside/GameSideView.kt
@@ -1,54 +1,83 @@
 package com.cretihoy.wordminded2.presentation.components.gameside
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.cretihoy.wordminded2.presentation.components.button.ButtonView
 import com.cretihoy.wordminded2.presentation.components.spacer.SpacerView
+import com.cretihoy.wordminded2.presentation.components.text.TextView
+import com.cretihoy.wordminded2.presentation.theme.EMPTY_STRING
+import com.cretihoy.wordminded2.presentation.theme.Shapes
+import com.cretihoy.wordminded2.presentation.theme.spacingLarge
 
 @Composable
 fun GameSideView(
+    icon: ImageVector? = null,
     model: GameSideModel? = null,
     modifier: Modifier = Modifier,
     actionTask: () -> Unit = {},
     actionLetter: () -> Unit = {}
 ) {
-    GameLayout(
-        model = model,
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
     ) {
-        ButtonView(
-            model = model?.task,
-            clickAction = actionTask,
-            modifier = it
-        )
-        SpacerView()
-        ButtonView(
-            model = model?.letter,
-            clickAction = actionLetter,
-            modifier = it
-        )
+        GameSideLayout(
+            model = model
+        ) {
+            ButtonView(
+                model = model?.task,
+                clickAction = actionTask,
+                modifier = it
+            )
+            SpacerView()
+            ButtonView(
+                model = model?.letter,
+                clickAction = actionLetter,
+                modifier = it
+            )
+        }
+        icon?.let {
+            Icon(
+                imageVector = it,
+                contentDescription = EMPTY_STRING,
+                modifier = Modifier
+                    .scale(2f)
+                    .clip(Shapes.large)
+                    .background(MaterialTheme.colorScheme.onError)
+                    .padding(spacingLarge)
+            )
+        }
     }
 }
 
 @Composable
-fun GameLayout(
+fun GameSideLayout(
     model: GameSideModel? = null,
     modifier: Modifier = Modifier,
     content: @Composable (modifier: Modifier) -> Unit = {}
 ) {
     if (model?.isHorizontal?.value == true) {
-        Row(modifier = modifier) {
-            content.invoke(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .align(Alignment.CenterVertically)
-            )
+        Column(modifier = modifier) {
+            Row(modifier = Modifier.weight(1f)) {
+                content.invoke(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                )
+            }
+            TextView(model.name)
         }
     } else {
         Column(modifier = modifier) {
@@ -56,8 +85,8 @@ fun GameLayout(
                 modifier = Modifier
                     .fillMaxSize()
                     .weight(1f)
-                    .align(Alignment.CenterHorizontally)
             )
+            TextView(model?.name)
         }
     }
 }
