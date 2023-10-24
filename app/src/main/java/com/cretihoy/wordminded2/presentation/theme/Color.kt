68d923de2260ef02988f155e140c2e9967bf8d88
cretihoy <sergzhio238@gmail.com>
2023-10-24 22:34:38 +0300

    add new colors, fix themes and create night themes

--- app/src/main/java/com/cretihoy/wordminded2/presentation/theme/Color.kt
+++ app/src/main/java/com/cretihoy/wordminded2/presentation/theme/Color.kt
@@ -3,8 +3,16 @@ package com.cretihoy.wordminded2.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 val White = Color(0xFFFAFAFA)
-val Black = Color(0xFF333333)
-val DarkBlue = Color(0xFF01579B)
-val DarkRed = Color(0xFFB71C1C)
-val LightBlue = Color(0xFF40C4FF)
-val LightRed = Color(0xFFFF5252)
+val Black = Color(0xFF212121)
+
+val LightRed = Color(0xFFF78DA4)
+val DarkRed = Color(0xFF9E2433)
+
+val LightBlue = Color(0xFF84E6FF)
+val DarkBlue = Color(0xFF4091B4)
+
+val LightYellow = Color(0xFFF7EC6E)
+val DarkYellow = Color(0xFFACB139)
+
+val LightGray = Color(0xFFBDBDBD)
+val DarkGray = Color(0xFF757575)
