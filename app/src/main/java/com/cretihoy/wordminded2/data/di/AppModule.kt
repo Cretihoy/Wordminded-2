4590e8bcacf1cb96eddca6fb4acacd5e7a6ea0a3
cretihoy <sergzhio238@gmail.com>
2023-10-24 22:38:35 +0300

    provide repository

--- app/src/main/java/com/cretihoy/wordminded2/data/di/AppModule.kt
+++ app/src/main/java/com/cretihoy/wordminded2/data/di/AppModule.kt
@@ -5,6 +5,9 @@ import androidx.room.Room
 import com.cretihoy.wordminded2.data.Storage
 import com.cretihoy.wordminded2.data.db.UserDao
 import com.cretihoy.wordminded2.data.db.UserDatabase
+import com.cretihoy.wordminded2.presentation.screens.users.UserMapper
+import com.cretihoy.wordminded2.presentation.screens.users.UserModelFactory
+import com.cretihoy.wordminded2.presentation.screens.users.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,6 +36,20 @@ object AppModule {
             context,
             UserDatabase::class.java, "database-name"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(
+        database: UserDao,
+        mapper: UserMapper,
+        factory: UserModelFactory
+    ): UserRepository {
+        return UserRepository(
+            database,
+            mapper,
+            factory
+        )
     }
 
     @Provides
